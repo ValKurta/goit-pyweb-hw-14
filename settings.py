714b88ab9514ee424ba310f865f4f2255f5d3287
ValKurta <valentyn.kurta@gmail.com>
2024-09-19 21:38:00 +0200

    version 1.2: 1. Argon2 for password 2. JWT(access_token and refresh_token) 3. 2FA 4. HTTPS 5. rate-limiting logic 6. .env 7. email verification 8. redis 9. CORS 10. Cloudinary

--- settings.py
+++ settings.py
@@ -8,8 +8,8 @@ class Settings:
     MAIL_USERNAME = os.getenv("MAIL_USERNAME")
     MAIL_PASSWORD = os.getenv("MAIL_PASSWORD")
     MAIL_FROM = os.getenv("MAIL_FROM")
-    CLOUDINARY_CLOUD_NAME = cloud_name
-    CLOUDINARY_API_KEY = 12345678
-    CLOUDINARY_API_SECRET = api_secret
+    CLOUDINARY_CLOUD_NAME = os.getenv("CLOUDINARY_CLOUD_NAME")
+    CLOUDINARY_API_KEY = os.getenv("CLOUDINARY_API_KEY")
+    CLOUDINARY_API_SECRET = os.getenv("CLOUDINARY_API_SECRET")
 
 settings = Settings()
